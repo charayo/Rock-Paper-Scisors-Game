6d9307a8519f1f624e6d5d256f9422571c306b52
charayo <69734231+charayo@users.noreply.github.com>
2021-02-26 15:55:02 -0700

    I was able to complete this during a class when a friend suggested it.
    Rock Paper and Scissors game.
    A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or sometimes "blunts scissors"), but will lose to one who has played paper ("paper covers rock"); a play of paper will lose to a play of scissors ("scissors cuts paper")

---
+++ app.js
@@ -0,0 +1,53 @@
+var clicks = document.querySelectorAll('[data-btn]');
+var human;
+var computer;
+clicks.forEach(element => {
+    function compCh(){
+        var ran =  Math.floor(Math.random()*3)+1;
+        if (ran == 1){
+            computer = "paper";
+        } else if(ran == 2){
+            computer = "rock";
+        } else {
+            computer = "scissors";
+        }
+        console.log("human has " + human,"computer has " + computer);
+       }
+    function test() {
+        human = element.id;
+        compCh();
+        if (human =="rock" && computer == "paper"){            
+            resMsg.innerHTML = "Paper covers rock. Computer wins!"
+            compScore.innerHTML ++;
+        } else if (human =="rock" && computer == "scissors"){           
+            resMsg.innerHTML = "Rock covers Scissors. You win!"
+            userScore.innerHTML ++;
+        } else if (human =="rock" && computer == "rock"){
+            resMsg.innerHTML = "Tie!";      
+        } 
+        //--------------------
+        if (human =="paper" && computer == "scissors"){            
+            compScore.innerHTML ++;
+            resMsg.innerHTML = "Scissors cuts paper. Computer wins!"
+        } else if (human =="paper" && computer == "rock"){            
+            resMsg.innerHTML = "Paper covers rock. You win!"
+            userScore.innerHTML ++;
+        } else if (human =="paper" && computer == "paper"){
+            resMsg.innerHTML = "Tie!";            
+        }
+        //------------------------
+        if (human =="scissors" && computer == "rock"){
+            resMsg.innerHTML = "Rock covers scissors. Computer wins!";            
+            compScore.innerHTML ++;
+        } else if (human =="scissors" && computer == "paper"){           
+            resMsg.innerHTML = "Scissors cuts paper. You win!";
+            userScore.innerHTML ++;
+        } else if (human =="scissors" && computer == "scissors"){
+            resMsg.innerHTML = "Tie!";
+        } 
+    }
+    
+    element.addEventListener("click", test)
+    
+    
+});
